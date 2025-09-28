1375195132f970c0ee4f81ccd79b708779ca25be
Akshat-pixel-ship-it <shubhdubey007sd@gmail.com>
2025-09-28 20:46:58 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,9 @@ const Footer = () => {
                 <Instagram className="w-5 h-5" />
               </a>
               <a
-                href="#"
+                href="https://www.youtube.com/@Aestheticadilifts"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="w-10 h-10 bg-yellow-500/20 rounded-full flex items-center justify-center text-yellow-400 hover:bg-yellow-500 hover:text-black transition-all duration-300"
               >
                 <Youtube className="w-5 h-5" />
